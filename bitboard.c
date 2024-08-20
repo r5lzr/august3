5efcc518e8662d67c226f1079244c111e89f8bbe
githubuser55555 <unreversable22@gmail.com>
2024-08-20 21:10:41 +0100

    Set bitboard datatype as unsigned long long, stored current bit mask in bitboard with initial bit states as 0

--- bitboard.c
+++ bitboard.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define U64 unsigned long long
+
+#define get_bit(bitboard, square) (bitboard & (1ULL << square))
+
+
+void show_bitboard(U64 bitboard)
+{
+  for (int rank = 0; rank < 8; rank++)
+  {
+    for (int file = 0; file < 8; file++)
+    {
+
+      int square = rank * 8 + file;
+
+      printf(" %d ", get_bit(bitboard, square) ? 1 : 0);
+
+    }
+
+    printf("\n");
+  }
+}
 
--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define U64 unsigned long long
+
+
 int main()
 {
-
-
+  U64 bitboard = 0ULL;
+  show_bitboard(bitboard);
 
   return 0;
 }
