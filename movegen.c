7a2e298f0adba13e1a22b244eba2866c0f4d9910
githubuser55555 <unreversable22@gmail.com>
2024-09-06 14:19:19 +0100

    Added checks for off board movement, if target square blocked, pawn promotion, pawn push and 2x pawn push

--- movegen.c
+++ movegen.c
@@ -71,10 +71,30 @@ void generate_moves(int side)
         while (bitboard)
         {
           source_square = __builtin_ctzll(bitboard);
-          printf("white pawn: %s\n", square_to_coordinates[source_square]);
 
           target_square = source_square - 8;
-          printf("white pawn: %s\n", square_to_coordinates[target_square]);
+
+          // prevents off board movement and if target square blocked
+          if (!(target_square < 0) && !(get_bit(side_bitboards[both], target_square)))
+          {
+            // pawn promo
+            if (source_square >= a7 && source_square <= h7)
+            {
+              printf("pawn promo: %s%sq\n", square_to_coordinates[source_square], square_to_coordinates[target_square]);
+              printf("pawn promo: %s%sr\n", square_to_coordinates[source_square], square_to_coordinates[target_square]);
+              printf("pawn promo: %s%sb\n", square_to_coordinates[source_square], square_to_coordinates[target_square]);
+              printf("pawn promo: %s%sn\n", square_to_coordinates[source_square], square_to_coordinates[target_square]);
+            }
+
+            else
+            {
+              printf("pawn push: %s%s\n", square_to_coordinates[source_square], square_to_coordinates[target_square]);
+
+              if ((source_square >= a2 && source_square <= h2) && !(get_bit(side_bitboards[both], target_square - 8)))
+                printf("2x pawn push: %s%s\n", square_to_coordinates[source_square], square_to_coordinates[target_square - 8]);
+
+            }
+          }
 
           pop_bit(bitboard, source_square);
         }
