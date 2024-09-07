9d4862e21afbd92315fc1ed76e16cd9428480f09
githubuser55555 <unreversable22@gmail.com>
2024-09-07 16:38:22 +0100

    Added checks for rook move and capture for both sides

--- movegen.c
+++ movegen.c
@@ -233,6 +233,35 @@ void generate_moves(FenBoard board)
         pop_bit(bitboard, source_square);
       }
     }
+
+    if ((piece == R && !board.side) || (piece == r && board.side))
+    {
+      while (bitboard)
+      {
+        source_square = __builtin_ctzll(bitboard);
+
+        attacks = get_rook_attacks(source_square, side_bitboards[both]) & ~side_bitboards[!board.side ? white : black];
+
+        while (attacks)
+        {
+          target_square = __builtin_ctzll(attacks);
+
+          if (!get_bit(side_bitboards[!board.side ? black : white], target_square))
+          {
+            printf("rook move: %s%s\n", square_to_coordinates[source_square], square_to_coordinates[target_square]);
+          }
+
+          else
+          {
+            printf("rook capture: %s%s\n", square_to_coordinates[source_square], square_to_coordinates[target_square]);
+          }
+
+          pop_bit(attacks, target_square);
+        }
+
+        pop_bit(bitboard, source_square);
+      }
+    }
   }
 }
 
