6ee5d04f59f83ed764bead115d1da040856b29dd
githubuser55555 <unreversable22@gmail.com>
2024-09-08 15:35:10 +0100

    Added macros to decode and get move items using respective hexidecimal constants of binary representation and opposing bit shifts

--- movegen.h
+++ movegen.h
@@ -3,12 +3,16 @@
 
 
 #define encode_move(source, target, piece, promoted, capture, double, enpassant, castling) \
-(source) | (target << 6) | (piece << 12) | (promoted << 16) | (capture << 20) | (double << 21) | (double << 22) | (castling << 23) \
+(source) | (target << 6) | (piece << 12) | (promoted << 16) | (capture << 20) | (double << 21) | (enpassant << 22) | (castling << 23) \
 
-
-
-
-
+#define get_move_source(move) (move & 0x3f)
+#define get_move_target(move) ((move & 0xfc0) >> 6)
+#define get_move_piece(move) ((move & 0xf000) >> 12)
+#define get_move_promoted(move) ((move & 0xf0000) >> 16)
+#define get_move_capture(move) ((move & 0x100000) >> 20)
+#define get_move_double(move) ((move & 0x200000) >> 21)
+#define get_move_enpassant(move) ((move & 0x400000) >> 22)
+#define get_move_castling(move) ((move & 0x800000) >> 23)
 
 void show_attacked_squares(int side);
 int is_square_attacked(int square, int side);
