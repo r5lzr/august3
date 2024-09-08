b6249941d88c70dbd4b94815b361394c25b4e90f
githubuser55555 <unreversable22@gmail.com>
2024-09-08 13:15:12 +0100

    Added macro to encode move items in binary format represented as a single integer

--- movegen.h
+++ movegen.h
@@ -2,6 +2,9 @@
 #define MOVE_GEN_H
 
 
+#define encode_move(source, target, piece, promoted, capture, double, enpassant, castling) \
+(source) | (target << 6) | (piece << 12) | (promoted << 16) | (capture << 20) | (double << 21) | (double << 22) | (castling << 23) \
+
 
 
 
@@ -10,5 +13,6 @@
 void show_attacked_squares(int side);
 int is_square_attacked(int square, int side);
 void generate_moves();
+
 
 #endif
