67cc3b632624889b8d2762c3737d6ebe583027c5
githubuser55555 <unreversable22@gmail.com>
2024-09-21 21:00:50 +0100

    Initialised search .c and .h files

---
+++ search.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bitboard.h"
+#include "search.h"
+#include "attacktable.h"
+#include "magicbitboard.h"
+#include "movegen.h"
+#include "util.h"
+
+
---
+++ search.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+
+
+
+
+#endif
+
