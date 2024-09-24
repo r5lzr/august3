4795d7f9b00defa6681c5f4aee0d448b595f5d31
githubuser55555 <unreversable22@gmail.com>
2024-09-24 18:59:09 +0100

    Defined mvv_lva table and node increment in quiescence
    - For capturing moves involving most valuable victim and least valuable attacker
    - Node increment to further track node count in quiescence

--- main.c
+++ main.c
@@ -15,11 +15,13 @@ int main()
   leaper_attacks_table();
   slider_attacks_table();
 
-  uci_loop();
+//  uci_loop();
 
-//  parse_fen(initial_position, &board);
-//  show_board();
-//  search_position(2);
+  parse_fen("r2q1rk1/ppp2ppp/2n1bn2/2b1p3/3pP3/3P1NPP/PPP1NPB1/R1BQ1RK1 b - - 0 9 ", &board);
+  show_board();
+//  search_position(3);
+
+  printf("move score: %d\n", mvv_lva[k][P]);
 
   return 0;
 }
--- search.h
+++ search.h
@@ -4,10 +4,12 @@
 
 extern int ply;
 extern int best_move;
+extern int mvv_lva[12][12];
 
 int quiescence(int alpha, int beta);
 int negamax(int alpha, int beta, int depth);
 void search_position(int depth);
+
 
 #endif
 
