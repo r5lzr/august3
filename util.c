7f32754f0a1d9596a622b2e2c9a94ef2afbba7f3
githubuser55555 <unreversable22@gmail.com>
2024-10-02 22:02:11 +0100

    Initialise time controls for UCI input handling
    - UCI works in either interactive console or pipeline for chess arena GUI
    - Console mode checks if any input events waiting and pipe mode checks if there's data from GUI
    - input_waiting handles both and is called periodically to see if input available w/o affecting performance
    - Checks for commands such as stopping search when allocated time is up or when GUI sends stop command

--- util.c
+++ util.c
@@ -1,9 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <unistd.h>
 #include "util.h"
+
+int quit = 0;
+int moves_to_go = 30;
+int move_time = -1;
+int time = -1;
+int inc_time = 0;
+int start_time = 0;
+int stop_time = 0;
+int time_set = 0;
+int stopped = 0;
 
 int get_time_ms()
 {
   return GetTickCount();
+}
+
+int input_waiting()
+{
+  static int init = 0, pipe;
+  static HANDLE inh;
+  DWORD dw;
+
+  if (!init)
+  {
+    init = 1;
+    inh = GetStdHandle(STD_INPUT_HANDLE);
+    pipe = !GetConsoleMode(inh, &dw);
+
+    if (!pipe)
+    {
+      SetConsoleMode(inh, dw & ~(ENABLE_MOUSE_INPUT|ENABLE_WINDOW_INPUT));
+      FlushConsoleInputBuffer(inh);
+    }
+  }
+
+  if (pipe)
+  {
+    if (!PeekNamedPipe(inh, NULL, 0, NULL, &dw, NULL))
+    {
+      return 1;
+    }
+
+    return dw;
+  }
+
+  else
+  {
+    GetNumberOfConsoleInputEvents(inh, &dw);
+    return dw <= 1 ? 0 : dw;
+  }
 }
--- util.h
+++ util.h
@@ -2,7 +2,18 @@
 #define PERFT_H
 
 
+extern int quit;
+extern int moves_to_go;
+extern int move_time;
+extern int time;
+extern int inc_time;
+extern int start_time;
+extern int stop_time;
+extern int time_set;
+extern int stopped;
+
 int get_time_ms();
+int input_waiting();
 
 
 #endif
